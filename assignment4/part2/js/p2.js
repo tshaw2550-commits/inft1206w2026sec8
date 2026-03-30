0b43ef7e67e39d6f1b6cfd3a362b4fd1481aaeaa
DreadStorm51 <tshaw2550@gmail.com>
2026-03-30 13:28:14 -0400

    added comment header to p2.js

--- assignment4/part2/js/p2.js
+++ assignment4/part2/js/p2.js
@@ -1,3 +1,10 @@
+/*
+Name: Terrence Shaw
+File: p2.js
+Date: 30 March 2026
+Challenge: Image gallery
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
