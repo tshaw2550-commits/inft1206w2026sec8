d329717b7d71c8106407e11fcd31e0a475c80266
DreadStorm51 <tshaw2550@gmail.com>
2026-04-05 16:25:19 -0400

    added the images to the thumbnail bar

--- assignment4/part2/js/p2.js
+++ assignment4/part2/js/p2.js
@@ -19,3 +19,19 @@ const images = [
     {filename: "pic5.jpg", alt: "Large moth on a leaf"}
 ];
 
+const baseURL = " ";
+
+for (const image of images) {
+    const newImage = document.createElement("img");
+    newImage.src = `${baseURL}${image.filename}`;
+    newImage.alt = image.alt;
+    newImage.tabIndex = "0";
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", updateDisplayedImage);
+    newImage.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            updateDisplayedImage(e);
+        }
+    });
+
+}
