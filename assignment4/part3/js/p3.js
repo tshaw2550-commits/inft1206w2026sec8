06079986c74051e479cdc4edebc62bd10c1fcfa5
DreadStorm51 <tshaw2550@gmail.com>
2026-04-06 15:49:32 -0400

    fixed date

--- assignment4/part3/js/p3.js
+++ assignment4/part3/js/p3.js
@@ -1,46 +1,6 @@
 /*
 Name: Terrence Shaw
 File: p3.js
-Date: 5 April 2026
+Date: 6 April 2026
 Object building practice
 */
-
-// setup canvas
-
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
-
-// function to generate random number
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-}
-class Ball {
-  // …
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-}
