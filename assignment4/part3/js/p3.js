4456608280cf56f4e150def5fe0f4a13bf622959
DreadStorm51 <tshaw2550@gmail.com>
2026-04-06 16:00:20 -0400

    drawing the ball

--- assignment4/part3/js/p3.js
+++ assignment4/part3/js/p3.js
@@ -34,4 +34,12 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
+
