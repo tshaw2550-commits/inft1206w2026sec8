464563e9226456f1c14babea3556c0789b0f7b1e
DreadStorm51 <tshaw2550@gmail.com>
2026-04-06 15:50:55 -0400

    added js

--- assignment4/part3/js/p3.js
+++ assignment4/part3/js/p3.js
@@ -4,3 +4,23 @@ File: p3.js
 Date: 6 April 2026
 Object building practice
 */
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
